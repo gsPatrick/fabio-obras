f54bb1ff8adb9fd49cd0a0c66c3fe6dd2a696bc7
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2025-09-26 05:19:09 -0300

    front3

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ class App {
     try {
       await db.sequelize.authenticate();
       console.log('✅ Conexão com o banco de dados estabelecida com sucesso.');
-      await db.sequelize.sync({ alter: true });
+      await db.sequelize.sync({ force: true });
       console.log('🔄 Modelos sincronizados com o banco de dados.');
       await this.seedAdminUser();
       await this.seedCategories();

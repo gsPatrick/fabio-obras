652d17239ddf8ae8b71eb8d68bedfc8d2560024f
Patrick Siqueira <patrickgsiqueira@hotmail.com>
2025-10-01 20:44:47 -0300

    mudanca

--- src/config/mercadoPago.js
+++ src/config/mercadoPago.js
@@ -1,6 +1,5 @@
 const mercadopago = require("mercadopago")
 
-// Garante que o token de acesso do Mercado Pago seja lido do ambiente
 mercadopago.configure({
   access_token: process.env.MERCADO_PAGO_TOKEN,
 })
